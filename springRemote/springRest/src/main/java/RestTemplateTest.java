2cb1ef14c728ef7adb9838f104da7a3773eb3937
Alexander <aprilipko.pragmasoft.com.ua>
2017-03-21 18:45:12 +0200

    RESTful-WS in spring - in process

---
+++ springRemote/springRest/src/main/java/RestTemplateTest.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Alexander
+ * 21.03.2017
+ */
+public class RestTemplateTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
+//        RestTemplate restTemplate = context.getBean("restTemplate", RestTemplate.class);
+//        restTemplate.
+    }
+}
