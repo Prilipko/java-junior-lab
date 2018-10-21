87cd9e71f00d84ea2b7beb48df928be92ba3a9cf
Oleksandr Prylypko <prilipko.alexander@gmail.com>
2018-10-21 15:14:54 +0300

    kafka-arg creation

---
+++ kafka-agr/src/main/java/com/example/kafkaagr/KafkaAgrApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafkaagr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAgrApplication.class, args);
+    }
+}
---
+++ kafka-agr/src/test/java/com/example/kafkaagr/KafkaAgrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kafkaagr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaAgrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
